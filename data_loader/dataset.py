cfe9d322cfb92d132c617c421d36dd97c06e1993
luotao <luotao@renrenche.com>
2019-04-24 17:20:27 +0800

    针对ICDAR2015的适配

--- data_loader/dataset.py
+++ data_loader/dataset.py
@@ -1,8 +1,10 @@
-from torch.utils.data import Dataset
-from .datautils import *
-import scipy.io as sio
 import logging
-import numpy as np
+from itertools import compress
+
+import scipy.io as sio
+from torch.utils.data import Dataset
+
+from .datautils import *
 
 logger = logging.getLogger(__name__)
 
@@ -14,15 +16,16 @@ class MyDataset(Dataset):
         self.txt_root = txt_root
 
     def __getitem__(self, index):
-
-        img_path,img, score_map, geo_map, training_mask, text,bboxes = image_label(self.txt_root,
-                                                                   self.image_list, self.img_name, index,
-                                                                   input_size=512,
-                                                                   random_scale=np.array([0.5, 1, 2.0, 3.0]),
-                                                                   background_ratio=3. / 8)
+        img_path, img, score_map, geo_map, training_mask, text, bboxes = image_label(self.txt_root,
+                                                                                     self.image_list, self.img_name,
+                                                                                     index,
+                                                                                     input_size=512,
+                                                                                     random_scale=np.array(
+                                                                                         [0.5, 1, 2.0, 3.0]),
+                                                                                     background_ratio=3. / 8)
         if len(bboxes) == 0:
             return self.__getitem__(index)
-        return img_path,img, score_map, geo_map, training_mask, text,bboxes
+        return img_path, img, score_map, geo_map, training_mask, text, bboxes
 
     def __len__(self):
         return len(self.image_list)
@@ -102,8 +105,9 @@ class ICDAR(Dataset):
 
         try:
             return self.__transform((imageName, bboxes, transcripts))
-        except:
-            return self.__getitem__(np.random.randint(0, len(self)))
+        except Exception as e:
+            # tb.print_exc(file=sys.stdout)
+            return self.__getitem__(torch.tensor(np.random.randint(0, len(self))))
 
     def __len__(self):
         return len(self.images)
@@ -111,7 +115,9 @@ class ICDAR(Dataset):
     def __transform(self, gt, input_size=512, random_scale=np.array([0.5, 1, 2.0, 3.0]),
                     background_ratio=3. / 8):
         """
+
         :param gt: image path (str), wordBBoxes (2 * 4 * num_words), transcripts (multiline)
+
         :return:
         """
 
@@ -120,26 +126,28 @@ class ICDAR(Dataset):
         # wordBBoxes = np.expand_dims(wordBBoxes, axis = 2) if (wordBBoxes.ndim == 2) else wordBBoxes
         # _, _, numOfWords = wordBBoxes.shape
         numOfWords = len(wordBBoxes)
-        text_polys = wordBBoxes  # num_words * 8
+        text_polys = wordBBoxes  # num_words * 4 * 2
         transcripts = [word for line in transcripts for word in line.split()]
         text_tags = [True if (tag == '*' or tag == '###') else False for tag in transcripts]  # ignore '###'
 
         if numOfWords == len(transcripts):
             h, w, _ = im.shape
-            text_polys, transcripts, text_tags = check_and_validate_polys(text_polys, transcripts, text_tags, (h, w))
+            text_polys, _, text_tags = check_and_validate_polys(text_polys, transcripts, text_tags, (h, w))
 
             rd_scale = np.random.choice(random_scale)
             im = cv2.resize(im, dsize=None, fx=rd_scale, fy=rd_scale)
             text_polys *= rd_scale
 
+            rectangles = []
+
             # print rd_scale
             # random crop a area from image
             if np.random.rand() < background_ratio:
                 # crop background
-                im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=True)
+                im, text_polys, text_tags, selected_poly = crop_area(im, text_polys, text_tags, crop_background=True)
                 if text_polys.shape[0] > 0:
                     # cannot find background
-                    raise TypeError('cannot find background')
+                    raise RuntimeError('cannot find background')
                 # pad and resize image
                 new_h, new_w, _ = im.shape
                 max_h_w_i = np.max([new_h, new_w, input_size])
@@ -148,13 +156,12 @@ class ICDAR(Dataset):
                 im = cv2.resize(im_padded, dsize=(input_size, input_size))
                 score_map = np.zeros((input_size, input_size), dtype=np.uint8)
                 geo_map_channels = 5
-                #                     geo_map_channels = 5 if FLAGS.geometry == 'RBOX' else 8
                 geo_map = np.zeros((input_size, input_size, geo_map_channels), dtype=np.float32)
                 training_mask = np.ones((input_size, input_size), dtype=np.uint8)
             else:
-                im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=False)
+                im, text_polys, text_tags, selected_poly = crop_area(im, text_polys, text_tags, crop_background=False)
                 if text_polys.shape[0] == 0:
-                    raise TypeError('cannot find background')
+                    raise RuntimeError('cannot find background')
                 h, w, _ = im.shape
 
                 # pad the image to the training input size or the longer side of image
@@ -173,17 +180,27 @@ class ICDAR(Dataset):
                 text_polys[:, :, 0] *= resize_ratio_3_x
                 text_polys[:, :, 1] *= resize_ratio_3_y
                 new_h, new_w, _ = im.shape
-                score_map, geo_map, training_mask = generate_rbox((new_h, new_w), text_polys, text_tags)
+                score_map, geo_map, training_mask, rectangles = generate_rbox((new_h, new_w), text_polys, text_tags)
 
-            # predict 出来的feature map 是 128 * 128， 所以 gt 需要取 /4 步长
+            step_size = int(input_size // 128)
+
             images = im[:, :, ::-1].astype(np.float32)  # bgr -> rgb
-            score_maps = score_map[::4, ::4, np.newaxis].astype(np.float32)
-            geo_maps = geo_map[::4, ::4, :].astype(np.float32)
-            training_masks = training_mask[::4, ::4, np.newaxis].astype(np.float32)
+            score_maps = score_map[::step_size, ::step_size, np.newaxis].astype(np.float32)
+            geo_maps = geo_map[::step_size, ::step_size, :].astype(np.float32)
+            training_masks = training_mask[::step_size, ::step_size, np.newaxis].astype(np.float32)
 
-            return images, score_maps, geo_maps, training_masks, transcripts
+            transcripts = [transcripts[i] for i in selected_poly]
+            mask = [not (word == '*' or word == '###') for word in transcripts]
+            transcripts = list(compress(transcripts, mask))
+            rectangles = list(compress(rectangles, mask))  # [ [pt1, pt2, pt3, pt3],  ]
+
+            assert len(transcripts) == len(rectangles)  # make sure length of transcipts equal to length of boxes
+            if len(transcripts) == 0:
+                raise RuntimeError('No text found.')
+
+            return imagePath, images, score_maps, geo_maps, training_masks, transcripts, rectangles
         else:
-            raise TypeError('Number of bboxes is inconsist with number of transcripts ')
+            raise TypeError('Number of bboxes is inconsistent with number of transcripts ')
 
 
 class SynthTextDataset(Dataset):
